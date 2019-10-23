cd00617c1c7ac2989892a2cc94ba226727679d12
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-10-22 17:11:27 -0700

    demo equivalent

--- src/components/Supplements.js
+++ src/components/Supplements.js
@@ -43,15 +43,88 @@ export const Supplements = ({ appType, label, supplements }) => {
       </Box>
 
       {show && (
-        <Box as='ul' margin={{ horizontal: 'small' }} pad={{ left: 'medium' }}>
+        <Box as='ul' margin={{ bottom: 'medium' }}>
           {supplements
             .filter(supp =>
               label === 'Required' ? !supp.optional : supp.optional
             )
             .filter(supp => supp.applications.includes(appType))
             .map(essay => (
-              <Box as='li' key={essay.name} style={{ listStyle: 'none' }}>
-                <Text>{essay.name}</Text>
+              <Box as='li' key={essay.name} margin={{ bottom: 'medium' }}>
+                {!!essay.instructions && (
+                  <Box margin={{ bottom: 'small', horizontal: 'small' }}>
+                    <Text weight='bold' margin={{ bottom: 'small' }}>
+                      Instructions:
+                    </Text>
+                    <Text
+                      margin={
+                        essay.instructions.includes('<p>')
+                          ? { top: '-20px' }
+                          : 'none'
+                      }
+                      dangerouslySetInnerHTML={{ __html: essay.instructions }}
+                    />
+                  </Box>
+                )}
+                <Box direction='row' justify='between'>
+                  <Box width='small' margin={{ horizontal: 'small' }}>
+                    <Box
+                      border={{
+                        side: 'bottom',
+                        size: 'small',
+                        color: '#2DA7A1',
+                      }}
+                      margin={{ bottom: 'small' }}
+                    >
+                      <Text weight='bold'>Details</Text>
+                    </Box>
+                    <Text>
+                      {essay.display_length.includes('words')
+                        ? `${essay.display_length} max`
+                        : essay.display_length}
+                    </Text>
+                  </Box>
+                  <Box width='large' margin={{ horizontal: 'small' }}>
+                    <Box
+                      direction='row'
+                      align='center'
+                      border={{
+                        side: 'bottom',
+                        size: 'small',
+                        color: '#2DA7A1',
+                      }}
+                      margin={{ bottom: 'small' }}
+                    >
+                      <Text weight='bold'>{essay.name}</Text>
+                      <Box
+                        margin={{ left: 'small' }}
+                        height='18px'
+                        justify='center'
+                        background='#2DA7A4'
+                        round='small'
+                        pad={{ horizontal: 'xsmall' }}
+                      >
+                        <Text color='white' weight='bold' size='xsmall'>
+                          {essay.last_updated === new Date().getFullYear()
+                            ? 'Current'
+                            : essay.last_updated}
+                        </Text>
+                      </Box>
+                    </Box>
+                    <Box as='ul'>
+                      {essay.prompts.map(({ prompt }) => (
+                        <Box
+                          margin={
+                            prompt.includes('<p>') ? { top: '-20px' } : 'none'
+                          }
+                          dangerouslySetInnerHTML={{ __html: prompt }}
+                          as='li'
+                          key={prompt}
+                        />
+                      ))}
+                    </Box>
+                  </Box>
+                </Box>
               </Box>
             ))}
         </Box>
